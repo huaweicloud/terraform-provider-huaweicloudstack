13dc8b1c9d86d7d22b45dd8befe5374448857430
ShiChangkuo <shichangkuo90@gmail.com>
2019-12-26 20:05:51 +0800

    Support huaweicloudstack_lb_certificate_v2 resource

--- huaweicloudstack/provider.go
+++ huaweicloudstack/provider.go
@@ -167,6 +167,7 @@ func Provider() terraform.ResourceProvider {
 			"huaweicloudstack_compute_servergroup_v2":             resourceComputeServerGroupV2(),
 			"huaweicloudstack_compute_floatingip_associate_v2":    resourceComputeFloatingIPAssociateV2(),
 			"huaweicloudstack_compute_volume_attach_v2":           resourceComputeVolumeAttachV2(),
+			"huaweicloudstack_lb_certificate_v2":                  resourceCertificateV2(),
 			"huaweicloudstack_lb_loadbalancer_v2":                 resourceLoadBalancerV2(),
 			"huaweicloudstack_lb_listener_v2":                     resourceListenerV2(),
 			"huaweicloudstack_lb_pool_v2":                         resourcePoolV2(),
